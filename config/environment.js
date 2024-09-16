73575eabce44f266772f84c3141d24c241439b1e
mirdono <alexvdberghe@fastmail.com>
2024-09-16 14:43:20 +0200

    chore: boilerplate

--- config/environment.js
+++ config/environment.js
@@ -18,6 +18,7 @@ module.exports = function (environment) {
       // Here you can pass flags/options to your application instance
       // when it is created
     },
+    appName: 'Cycling form group 7',
   };
 
   if (environment === 'development') {
